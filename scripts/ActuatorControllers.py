a3915d41796ca929f23c0470d5416c7aff27f883
Patrick Gavigan <pgavigan@gmail.com>
2020-03-12 22:32:35 -0400

    Fixed silly typo in message names

--- scripts/ActuatorControllers.py
+++ scripts/ActuatorControllers.py
@@ -4,18 +4,21 @@
 # @author: Patrick Gavigan
 
 import rospy
-from std_msgs.msg import float64
+from std_msgs.msg import Float64
 
 def commnadBrake(data, airSimConnector):
+    rospy.loginfo("brake message: " + str(data.data))
     airSimConnector.brake(data.data)
 
 def commandThrottle(data, airSimConnector):
+    rospy.loginfo("throttle message: " + str(data.data))
     airSimConnector.throttle(data.data)
     
 def commandSteering(data, airSimConnector):
+    rospy.loginfo("steering message: " + str(data.data))
     airSimConnector.steering(data.data)
 
 def runActuators(airSimConnector):
-    rospy.Subscriber('carCommnad/Brake', float64, commnadBrake, airSimConnector)
-    rospy.Subscriber('carCommnad/Throttle', float64, commandThrottle, airSimConnector)
-    rospy.Subscriber('carCommnad/Steering', float64, commandSteering, airSimConnector)
+    rospy.Subscriber('carCommand/Brake', Float64, commnadBrake, airSimConnector)
+    rospy.Subscriber('carCommand/Throttle', Float64, commandThrottle, airSimConnector)
+    rospy.Subscriber('carCommand/Steering', Float64, commandSteering, airSimConnector)
